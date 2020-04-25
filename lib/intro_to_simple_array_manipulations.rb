732936920099b7ae4735a9bde0e057a670ddeca9
Paul Ralyea <047par@gmail.com>
2020-04-25 04:37:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,9 @@ def using_concat(array_1,array_2)
   new_array = array_1.concat(array_2)
 end
  
-def using_insert
-  
+def using_insert(array, element)
+  array.insert(element, 3)
+end
 #def using_uniq
   
 #def using_flatten
